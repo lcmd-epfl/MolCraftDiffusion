00a72c41189bc1178fb1ed702449ed670577430a
pregHosh <iflp1908sl@gmail.com>
2026-02-24 15:22:49 +0100

    include get_versioned_output_path in the train module

--- src/MolecularDiffusion/runmodes/train/__init__.py
+++ src/MolecularDiffusion/runmodes/train/__init__.py
@@ -3,11 +3,12 @@ from .logger import Logger
 from .tasks_egcl import ModelTaskFactory as ModelTaskFactory_EGCL
 from .tasks_egt import ModelTaskFactory as ModelTaskFactory_EGT
 from .trainer import OptimSchedulerFactory
-from .eval import evaluate
+from .eval import evaluate, get_versioned_output_path
 
 __all__ = ['DataModule', 
            'Logger',
            'ModelTaskFactory_EGCL',
            'ModelTaskFactory_EGT', 
            'OptimSchedulerFactory', 
+           "get_versioned_output_path",
            'evaluate']
